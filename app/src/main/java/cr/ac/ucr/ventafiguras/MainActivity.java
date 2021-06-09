bea5091db3daf4141658fdb18b528e20438fb039
= <=>
2021-06-09 13:41:45 -0600

    no redirection

--- app/src/main/java/cr/ac/ucr/ventafiguras/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ventafiguras/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView) findViewById(R.id.webViewMain);
+        webView.setWebViewClient(new WebViewClient());
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
